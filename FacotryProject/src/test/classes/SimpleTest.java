52721cb6d8f84b1d17890e34a0f542c718fd330f
liujingcheng <cyliujingcheng@126.com>
2017-09-05 22:51:19 +0800

    再加一些Sample

---
+++ FacotryProject/src/test/classes/SimpleTest.java
@@ -0,0 +1,11 @@
+package test.classes;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void simpleTest() {
+		System.out.println("Simple Test Method.");
+	}
+}
---
+++ FacotryProject/src/test/factory/SimpleTestFactory.java
@@ -0,0 +1,13 @@
+package test.factory;
+
+import org.testng.annotations.Factory;
+
+import test.classes.SimpleTest;
+
+public class SimpleTestFactory {
+
+	@Factory
+	public Object[] factoryMethod() {
+		return new Object[] { new SimpleTest(), new SimpleTest() };
+	}
+}
